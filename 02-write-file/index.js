013a771a572b847c75c39eafeacccaa403c342a4
Nadzeya Amialchuk <timoshenkova.nadya@gmail.com>
2024-01-18 10:31:03 +0300

    feat: done second task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
 
+const file = './02-write-file/text.txt';
+process.stdin.setEncoding('utf8');
+console.log('Enter your text here: ');
+process.stdin.on('readable', () => {
+  let chunk;
+  while ((chunk = process.stdin.read()) !== null) {
+    if (chunk.trim() === 'exit') {
+      console.log('Thanks for writing!');
+      process.exit();
+    } else {
+      fs.appendFile(file, chunk, (err) => {
+        if (err) throw err;
+        console.log('Enter your text here: ');
+      });
+    }
+  }
+});
+
+process.stdin.on('end', () => {
+  process.exit();
+});
+process.on('SIGINT', () => {
+    console.log('Thanks for writing!');
+    process.exit();
+  });
