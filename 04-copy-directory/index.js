31f738a9ee7480013f952537cc8ec5b576e60e5e
Nadzeya Amialchuk <timoshenkova.nadya@gmail.com>
2024-01-18 14:25:58 +0300

    feat: done forth task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderOrigin = '04-copy-directory/files';
+const folderCopy = '04-copy-directory/files-copy';
+
+
+fs.mkdir(folderCopy, { recursive: true }, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+
+  fs.readdir(folderOrigin, (err, files) => {
+    if (err) {
+      console.error(err);
+    } else {
+      files.forEach(file => {
+        const originFilePath = path.join(folderOrigin, file);
+        const copyFilePath = path.join(folderCopy, file);
+
+        fs.stat(originFilePath, (err, stats) => {
+          if (err) {
+            console.error(err);
+          } else if (stats.isFile()) {
+            fs.copyFile(originFilePath, copyFilePath, (err) => {
+              if (err) {
+                console.error(err);
+              }
+            });
+          }
+        });
+      });
+    }
+  });
+});
