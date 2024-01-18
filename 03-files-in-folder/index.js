127ee26f8a6e75b0c81b10ceddec3cdb2607a946
Nadzeya Amialchuk <timoshenkova.nadya@gmail.com>
2024-01-18 12:01:06 +0300

    feat: done third task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(
+  '03-files-in-folder/secret-folder',
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) {
+      console.error('Error: ', err);
+    } else {
+      files.forEach((file) => {
+        if (file.isFile()) {
+          const filePath = path.join(
+            '03-files-in-folder/secret-folder',
+            file.name,
+          );
+          fs.stat(filePath, (err, stats) => {
+            if (err) {
+              console.error('Error: ', err);
+            } else {
+              const fileSize = stats.size / 1024;
+              let fileExt = path.extname(file.name);
+              fileExt = fileExt.slice(1);
+              const fileNameWithoutExt = path.basename(
+                file.name,
+                path.extname(file.name),
+              );
+              console.log(
+                `${fileNameWithoutExt}-${fileExt}-${fileSize}kb`,
+              );
+            }
+          });
+        }
+      });
+    }
+  },
+);
