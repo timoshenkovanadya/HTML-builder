984f9bf58dd7ef0847512c2bc0506bd5d273d06a
Nadzeya Amialchuk <timoshenkova.nadya@gmail.com>
2024-01-22 22:40:20 +0300

    fix: fix formatting in task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,7 +4,6 @@ const path = require('path');
 const folderOrigin = '04-copy-directory/files';
 const folderCopy = '04-copy-directory/files-copy';
 
-
 fs.mkdir(folderCopy, { recursive: true }, (err) => {
   if (err) {
     return console.error(err);
@@ -14,7 +13,7 @@ fs.mkdir(folderCopy, { recursive: true }, (err) => {
     if (err) {
       console.error(err);
     } else {
-      files.forEach(file => {
+      files.forEach((file) => {
         const originFilePath = path.join(folderOrigin, file);
         const copyFilePath = path.join(folderCopy, file);
 
@@ -34,7 +33,7 @@ fs.mkdir(folderCopy, { recursive: true }, (err) => {
         if (err) {
           console.error(err);
         } else {
-          filesCopy.forEach(fileCopy => {
+          filesCopy.forEach((fileCopy) => {
             if (!files.includes(fileCopy)) {
               fs.unlink(path.join(folderCopy, fileCopy), (err) => {
                 if (err) {
